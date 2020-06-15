2b6b81f74c22ffd8141c6268a9612ef37025ee3c
ClinteRich <clint.richardson@appliedis.com>
2020-06-15 15:12:33 -0400

    deployment script

--- 201-private-endpoint-sql-from-appservice/azureDeploySQL.ps1
+++ 201-private-endpoint-sql-from-appservice/azureDeploySQL.ps1
@@ -2,4 +2,4 @@
 $RG = New-AzResourceGroup -Name "AISU-E2-RG-SQL" -Location "EastUS2"
 
 #We then kickoff the deployment
-New-AzResourceGroupDeployment -Name "Private_SQL_From_AppService" -ResourceGroupName $RG -TemplateUri "" -TemplateParameterUri ""
+New-AzResourceGroupDeployment -Name "Private_SQL_From_AppService" -ResourceGroupName $RG -TemplateUri "https://raw.githubusercontent.com/ClinteRich/azure-quickstart-templates/master/201-private-endpoint-sql-from-appservice/azuredeploy.json" -TemplateParameterUri "https://raw.githubusercontent.com/ClinteRich/azure-quickstart-templates/master/201-private-endpoint-sql-from-appservice/azuredeploy.parameters.json"
